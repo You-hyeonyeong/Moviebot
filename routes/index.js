9dadbee510f79c3b43851d9a494e9226c474d1ff
You-hyeonyeong <dbgusdud412@naver.com>
2019-08-11 21:45:28 +0900

    이미지 경로 오류

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 router.use('/movie', require('./movie/index'));
 router.use('/auth', require('./auth/index'));
 router.use('/main', require('./main/index'));
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+})
+router.get('/img', function(req, res, next) {
+  fs.readFile('logo_bg.png'), function(err,data){
+    res.end(data);
+  }  
 });
+
 
 
 module.exports = router;
